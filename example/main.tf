d6be5c96f733af96756941491aae43aac5d517a6
atsushi-815 <atsushi586@yahoo.co.jp>
2021-03-03 09:30:57 +0900

    7-network-error

--- example/main.tf
+++ example/main.tf
@@ -3,6 +3,14 @@ module "web_server" {
   instance_type = "t2.micro"
 }
 
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 output "public_dns" {
   value = module.web_server.public_dns
 }
